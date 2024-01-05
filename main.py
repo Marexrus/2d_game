961fc6c4dd08c7f60424504522a9b9e46944a041
Marex <mmarexplay@gmail.com>
2024-01-05 21:52:29 +0300

    firstcommit

---
+++ main.py
@@ -0,0 +1,31 @@
+import pygame
+import random
+import time
+from math import *
+
+
+WIDTH = 700
+HEIGHT = 700
+FPS = 144
+
+BLACK = (0, 0, 0)
+GRAY=(128,128,128)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+
+running = True
+while running:
+    screen.fill(GRAY)
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pygame.display.update()
+pygame.quit()
